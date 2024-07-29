ea38762e4db9140d921152ca35773741d90f62f8
xfshahrukh44 <ssidduit@gmail.com>
2024-07-29 23:14:05 +0500

    changes 29/7/24

--- app/Http/Controllers/SupportController.php
+++ app/Http/Controllers/SupportController.php
@@ -221,6 +221,7 @@ class SupportController extends Controller
                 return view('support.bookformatting', compact('data'));
             }elseif($check == 7){
                 $data = BookWriting::find($form_id);
+                dd($data);
                 return view('support.bookwriting', compact('data'));
             }elseif($check == 8){
                 $data = AuthorWebsite::find($form_id);
