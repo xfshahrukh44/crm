195163faf6c2741221a84d96b1b603049d36b26c
xfshahrukh44 <ssidduit@gmail.com>
2024-08-02 00:40:35 +0500

    support: dom breaking messages

--- resources/views/support/task/show.blade.php
+++ resources/views/support/task/show.blade.php
@@ -415,7 +415,7 @@
                                                         </span>
                                                             </div>
                                                             <div class="ul-widget3-body">
-                                                                {!! preg_replace('/<\/?div[^>]*>/', '', (nl2br($sub_tasks->description))) !!}
+                                                                {!! strip_tags(nl2br($sub_tasks->description)) !!}
                                                             </div>
                                                         </div>
                                                         @if(count($sub_tasks->subtask_message) != 0)
