384dd3c236d7b73cb95ce46ce858592a78944a35
xfshahrukh44 <ssidduit@gmail.com>
2024-08-23 04:10:55 +0500

    v3: project detail dynamic

--- resources/views/livewire/project-detail.blade.php
+++ resources/views/livewire/project-detail.blade.php
@@ -61,7 +61,7 @@
 
                                     <div class="col">
                                         <p style="font-size: medium;">
-                                            <a href="{{route('support.form', [ 'form_id' => $project->form_id , 'check' => $project->form_checker, 'id' => $project->id])}}">
+                                            <a target="_blank" href="{{route('support.form', [ 'form_id' => $project->form_id , 'check' => $project->form_checker, 'id' => $project->id])}}">
                                                 <i class="i-Receipt-4 text-info"></i>
                                                 <br />
                                                 View form
@@ -71,7 +71,7 @@
 
                                     <div class="col">
                                         <p style="font-size: medium;">
-                                            <a href="{{ route('create.task.by.project.id', ['id' => $project->id, 'name' => preg_replace('/[^A-Za-z0-9\-]/', '', strtolower(str_replace(' ', '-', $project->name))) ]) }}">
+                                            <a target="_blank" href="{{ route('create.task.by.project.id', ['id' => $project->id, 'name' => preg_replace('/[^A-Za-z0-9\-]/', '', strtolower(str_replace(' ', '-', $project->name))) ]) }}">
                                                 <i class="fas fa-plus text-success"></i>
                                                 <br />
                                                 Create task
@@ -84,27 +84,6 @@
                     </div>
                 </div>
             </div>
-
-            {{--        <div class="row mb-4">--}}
-            {{--            <div class="col-lg-12 col-md-12">--}}
-            {{--                <h2 class="ml-3">Active Tasks</h2>--}}
-            {{--            </div>--}}
-            {{--            @php--}}
-            {{--                if (\Illuminate\Support\Facades\Auth::user()->is_employee == 2) {--}}
-            {{--                    $task_index_route = 'admin.task.index';--}}
-            {{--                } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 6) {--}}
-            {{--                    $task_index_route = 'manager.task.index';--}}
-            {{--                } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 0) {--}}
-            {{--                    $task_index_route = 'sale.task.index';--}}
-            {{--                } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 4) {--}}
-            {{--                    $task_index_route = 'support.task';--}}
-            {{--                }--}}
-            {{--            @endphp--}}
-            {{--            <div class="col-lg-12 col-md-12">--}}
-            {{--                <a target="_blank" href="{{route($task_index_route, ['project_id' => $project->id])}}" class="btn btn-primary ml-3">View all tasks</a>--}}
-            {{--                <a href="{{route($task_index_route, ['project_id' => $project->id])}}" class="btn btn-primary ml-3">View all tasks</a>--}}
-            {{--            </div>--}}
-            {{--        </div>--}}
 
             @foreach($categories_with_active_tasks as $category_with_active_tasks)
                 @if(count($category_with_active_tasks['tasks']))
@@ -131,13 +110,13 @@
                                         <tbody>
                                         @php
                                             if (\Illuminate\Support\Facades\Auth::user()->is_employee == 2) {
-                                                $show_route = 'admin.task.show';
+                                                $show_url = 'admin.task.show';
                                             } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 6) {
-                                                $show_route = 'manager.task.show';
+                                                $show_url = 'manager.task.show';
                                             } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 0) {
-                                                $show_route = 'sale.task.show';
+                                                $show_url = 'sale.task.show';
                                             } else if (\Illuminate\Support\Facades\Auth::user()->is_employee == 4) {
-                                                $show_route = 'support.task.show';
+                                                $show_url = 'support.task.show';
                                             }
                                         @endphp
                                         @foreach($category_with_active_tasks['tasks'] as $task)
@@ -145,16 +124,14 @@
                                                 <td style="vertical-align: middle;">
                                                     <span class="badge badge-sm badge-dark">#{{$task->id}}</span>
                                                 </td>
-                                                {{--                                <td style="vertical-align: middle;"><a target="_blank" href="{{route($show_route, $task->id)}}">{!! \Illuminate\Support\Str::limit(strip_tags($task->description), 25, $end='...') !!}</a></td>--}}
-                                                <td style="vertical-align: middle;"><a href="{{route($show_route, $task->id)}}">{!! \Illuminate\Support\Str::limit(strip_tags($task->description), 25, $end='...') !!}</a></td>
+                                                <td style="vertical-align: middle;"><a href="{{route($show_url, $task->id)}}">{!! \Illuminate\Support\Str::limit(strip_tags($task->description), 25, $end='...') !!}</a></td>
                                                 <td style="vertical-align: middle;">{{$task->user->name}} {{$task->user->last_name}}</td>
                                                 <td style="vertical-align: middle;">{!! $task->project_status_badge() !!}</td>
                                                 <td style="vertical-align: middle;">
-                                                    {{--                                    <a target="_blank" href="{{route($show_route, $task->id)}}" class="btn btn-primary btn-icon btn-sm">--}}
-                                                    <a href="{{route($show_route, $task->id)}}" class="badge badge-primary badge-icon badge-sm">
+                                                    <a target="_blank" href="{{route($show_url, $task->id)}}" class="badge badge-primary badge-icon badge-sm">
                                                         <span class="ul-badge__icon"><i class="i-Eye"></i> View</span>
                                                     </a>
-                                                    <a href="{{route($show_route, $task->id) . '?show-message=true'}}" class="badge badge-info badge-icon badge-sm">
+                                                    <a target="_blank" href="{{route($show_url, $task->id) . '?show-message=true'}}" class="badge badge-info badge-icon badge-sm">
                                                         <span class="ul-badge__icon"><i class="i-Speach-Bubble-3"></i> Message</span>
                                                     </a>
                                                 </td>
@@ -187,50 +164,21 @@
                     <h5 class="modal-title" id="exampleModalCenterTitle-2">Assign Agent</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
                 </div>
-                <form action="{{ route('support.reassign.support') }}" method="post">
-                    @csrf
-                    <div class="modal-body">
-                        <input type="hidden" name="id" id="assign_id">
-                        <input type="hidden" name="form" id="form_id">
-                        <div class="form-group">
-                            <label class="col-form-label" for="agent-name-wrapper">Name:</label>
-                            <select name="agent_id" id="agent-name-wrapper" class="form-control">
+                <div class="modal-body">
+                    <input type="hidden" name="id" id="assign_id">
+                    <input type="hidden" name="form" id="form_id">
+                    <div class="form-group">
+                        <label class="col-form-label" for="agent-name-wrapper">Name:</label>
+                        <select name="agent_id" id="agent-name-wrapper" class="form-control">
 
-                            </select>
-                        </div>
+                        </select>
                     </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
-                        <button class="btn btn-primary ml-2" type="submit">Save changes</button>
-                    </div>
-                </form>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Close</button>
+                    <button class="btn btn-primary ml-2" id="btn_assignModel" type="submit">Save changes</button>
+                </div>
             </div>
         </div>
     </div>
-
-    <script>
-        function assignAgent(id, form, brand_id){
-            $('#agent-name-wrapper').html('');
-            var url = "{{ route('get-support-agents', ['brand_id' => 'temp']) }}";
-            url = url.replace('temp', brand_id);
-            $.ajax({
-                type:'GET',
-                url: url,
-                success:function(data) {
-                    var getData = data.data;
-                    for (var i = 0; i < getData.length; i++) {
-                        $('#agent-name-wrapper').append('<option value="'+getData[i].id+'">'+getData[i].name+ ' ' +getData[i].last_name+'</option>');
-                    }
-
-                    $('#agent-name-wrapper').select2();
-                }
-            });
-            $('#assignModel').find('#assign_id').val(id);
-            $('#assignModel').find('#form_id').val(form);
-            $('#assignModel').modal('show');
-        }
-        $(document).ready(function() {
-
-        });
-    </script>
 </div>
