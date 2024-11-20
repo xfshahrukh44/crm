cb75dcc01a4e7939e62c57c3097010f785f277df
Shahrukh Siddiqui <shahrukh.siddiqui@technifiedlabs.com>
2024-11-21 03:42:19 +0500

    pay with authorize: paid invoice logic

--- resources/views/client/invoice/pay-with-authorize.blade.php
+++ resources/views/client/invoice/pay-with-authorize.blade.php
@@ -33,6 +33,7 @@
     <link href="{{ asset('newglobal/css/datatables.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('newglobal/css/select2.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('newglobal/css/sweetalert2.min.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intro.js/7.2.0/introjs.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @stack('styles')
     <style>
@@ -104,93 +105,111 @@
                             @php
                                 $brand = \App\Models\Brand::find($invoice->brand);
                             @endphp
-                            <img src="{{asset($brand->logo)}}" alt="">
+                            <img style="max-width: 300px !important;" src="{{asset($brand->logo)}}" alt="">
                         </div>
                         <div class="col-md-12">
-                            <ul class="nav nav-tabs justify-content-end mb-4" id="myTab" role="tablist">
-                                <li class="nav-item"><a class="nav-link active" id="invoice-tab" data-toggle="tab" href="#invoice" role="tab" aria-controls="invoice" aria-selected="true">Invoice</a></li>
-                            </ul>
-                            <div class="card">
-                                <div class="tab-content" id="myTabContent" style="padding-top: 100px; padding-bottom: 200px;">
-                                    <div class="tab-pane fade show active" id="invoice" role="tabpanel" aria-labelledby="invoice-tab">
-                                        <div class="row">
-                                            <div class="col-md-6 offset-md-3">
-                                                <div class="row">
-                                                    <div class="col-md-12 form-group text-center">
-                                                        <h2>Invoice # {{$invoice->invoice_number}}</h2>
+                            @if($invoice->payment_status == 2)
+                                <div class="col-md-12 text-center">
+                                    <h1>
+                                        <i class="fa fa-check-double text-success"></i>
+                                    </h1>
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <h1 class="text-success">
+                                        <b>
+                                            THANK YOU!
+                                        </b>
+                                    </h1>
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <h4>Invoice #{{$invoice->invoice_number}} has been paid.</h4>
+                                </div>
+                            @else
+                                <ul class="nav nav-tabs justify-content-end mb-4" id="myTab" role="tablist">
+                                    <li class="nav-item"><a class="nav-link active" id="invoice-tab" data-toggle="tab" href="#invoice" role="tab" aria-controls="invoice" aria-selected="true">Invoice</a></li>
+                                </ul>
+                                <div class="card">
+                                    <div class="tab-content" id="myTabContent" style="padding-top: 100px; padding-bottom: 200px;">
+                                        <div class="tab-pane fade show active" id="invoice" role="tabpanel" aria-labelledby="invoice-tab">
+                                            <div class="row">
+                                                <div class="col-md-6 offset-md-3">
+                                                    <div class="row">
+                                                        <div class="col-md-12 form-group text-center">
+                                                            <h2>Invoice # {{$invoice->invoice_number}}</h2>
+                                                        </div>
+                                                        <div class="col-md-4 form-group">
+                                                            <label for=""><h5>Package</h5></label>
+                                                            <h6>{{ $invoice->package == 0 ? $invoice->custom_package : $invoice->package }}</h6>
+                                                        </div>
+                                                        <div class="col-md-4 form-group">
+                                                            <label for=""><h5>Brand</h5></label>
+                                                            <h6>{{$brand->name}}</h6>
+                                                        </div>
+                                                        @php
+                                                            $service_list = explode(',', $invoice->service);
+                                                        @endphp
+                                                        <div class="col-md-4 form-group">
+                                                            <label for=""><h5>Service(s)</h5></label>
+                                                            <br>
+                                                            @for($i = 0; $i < count($service_list); $i++)
+                                                                <span class="btn btn-info btn-sm">{{ $invoice->services($service_list[$i])->name }}</span>
+                                                            @endfor
+                                                        </div>
+                                                        <div class="col-md-4 form-group">
+                                                            <label for=""><h5>Payment type</h5></label>
+                                                            <h6>{{ $invoice->payment_type_show() }}</h6>
+                                                        </div>
+                                                        <div class="col-md-4 form-group">
+                                                            <label for=""><h5>Amount</h5></label>
+                                                            <h6>{{$invoice->currency_show->sign}} {{ $invoice->amount }}</h6>
+                                                        </div>
                                                     </div>
-                                                    <div class="col-md-4 form-group">
-                                                        <label for=""><h5>Package</h5></label>
-                                                        <h6>{{ $invoice->package == 0 ? $invoice->custom_package : $invoice->package }}</h6>
-                                                    </div>
-                                                    <div class="col-md-4 form-group">
-                                                        <label for=""><h5>Brand</h5></label>
-                                                        <h6>{{$brand->name}}</h6>
-                                                    </div>
-                                                    @php
-                                                        $service_list = explode(',', $invoice->service);
-                                                    @endphp
-                                                    <div class="col-md-4 form-group">
-                                                        <label for=""><h5>Service(s)</h5></label>
-                                                        <br>
-                                                        @for($i = 0; $i < count($service_list); $i++)
-                                                            <span class="btn btn-info btn-sm">{{ $invoice->services($service_list[$i])->name }}</span>
-                                                        @endfor
-                                                    </div>
-                                                    <div class="col-md-4 form-group">
-                                                        <label for=""><h5>Payment type</h5></label>
-                                                        <h6>{{ $invoice->payment_type_show() }}</h6>
-                                                    </div>
-                                                    <div class="col-md-4 form-group">
-                                                        <label for=""><h5>Amount</h5></label>
-                                                        <h6>{{$invoice->currency_show->sign}} {{ $invoice->amount }}</h6>
-                                                    </div>
-                                                </div>
 
-                                                <form action="{{route('client.pay.with.authorize.submit', $invoice->id)}}" method="POST">
-                                                    @csrf
-                                                    <div class="row mt-4">
-                                                        <div class="col-md-12 form-group">
-                                                            <label for="">Card number</label>
-                                                            <input class="form-control" type="text" name="card_number" required>
+                                                    <form action="{{route('client.pay.with.authorize.submit', $invoice->id)}}" method="POST">
+                                                        @csrf
+                                                        <div class="row mt-4">
+                                                            <div class="col-md-12 form-group">
+                                                                <label for="">Card number</label>
+                                                                <input class="form-control" type="text" name="card_number" required>
+                                                            </div>
+                                                            <div class="col-md-4 form-group">
+                                                                <label for="">Expiration month</label>
+                                                                <select class="form-control" name="exp_month" id="" required>
+                                                                    <option value="1">1</option>
+                                                                    <option value="2">2</option>
+                                                                    <option value="3">3</option>
+                                                                    <option value="4">4</option>
+                                                                    <option value="5">5</option>
+                                                                    <option value="6">6</option>
+                                                                    <option value="7">7</option>
+                                                                    <option value="8">8</option>
+                                                                    <option value="9">9</option>
+                                                                    <option value="10">10</option>
+                                                                    <option value="11">11</option>
+                                                                    <option value="12">12</option>
+                                                                </select>
+                                                            </div>
+                                                            <div class="col-md-4 form-group">
+                                                                <label for="">Expiration year</label>
+                                                                <input class="form-control" type="number" name="exp_year" min="{{\Carbon\Carbon::now()->format('Y')}}" value="{{\Carbon\Carbon::now()->format('Y')}}" required>
+                                                            </div>
+                                                            <div class="col-md-4 form-group">
+                                                                <label for="">CVV</label>
+                                                                <input class="form-control" type="number" name="cvv" required>
+                                                            </div>
+                                                            <div class="col-md-12 form-group mt-2">
+                                                                <button type="submit" class="btn btn-primary btn-block">
+                                                                    Pay {{$invoice->currency_show->sign}}{{ $invoice->amount }}
+                                                                </button>
+                                                            </div>
                                                         </div>
-                                                        <div class="col-md-4 form-group">
-                                                            <label for="">Expiration month</label>
-                                                            <select class="form-control" name="exp_month" id="" required>
-                                                                <option value="1">1</option>
-                                                                <option value="2">2</option>
-                                                                <option value="3">3</option>
-                                                                <option value="4">4</option>
-                                                                <option value="5">5</option>
-                                                                <option value="6">6</option>
-                                                                <option value="7">7</option>
-                                                                <option value="8">8</option>
-                                                                <option value="9">9</option>
-                                                                <option value="10">10</option>
-                                                                <option value="11">11</option>
-                                                                <option value="12">12</option>
-                                                            </select>
-                                                        </div>
-                                                        <div class="col-md-4 form-group">
-                                                            <label for="">Expiration year</label>
-                                                            <input class="form-control" type="number" name="exp_year" min="{{\Carbon\Carbon::now()->format('Y')}}" value="{{\Carbon\Carbon::now()->format('Y')}}" required>
-                                                        </div>
-                                                        <div class="col-md-4 form-group">
-                                                            <label for="">CVV</label>
-                                                            <input class="form-control" type="number" name="cvv" required>
-                                                        </div>
-                                                        <div class="col-md-12 form-group mt-2">
-                                                            <button type="submit" class="btn btn-primary btn-block">
-                                                                Pay {{$invoice->currency_show->sign}}{{ $invoice->amount }}
-                                                            </button>
-                                                        </div>
-                                                    </div>
-                                                </form>
+                                                    </form>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            @endif
                         </div>
                     </div><!-- end of main-content -->
                 </div>
@@ -210,4 +229,14 @@
             </div>
         </div>
     </div>
+    @if(session()->has('success'))
+        <script>
+            toastr.success("{{session()->get('success')}}");
+        </script>
+    @endif
+    @if(session()->has('error'))
+        <script>
+            toastr.error("{{session()->get('error')}}");
+        </script>
+    @endif
 </body>
