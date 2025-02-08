34ed9867481fbe798be94fcf246ce969c7d8d151
Shahrukh Siddiqui <shahrukh.siddiqui@technifiedlabs.com>
2025-02-08 05:40:55 +0500

    admin, buh: revenue screen

--- app/Http/Livewire/Revenue.php
+++ app/Http/Livewire/Revenue.php
@@ -48,10 +48,8 @@ class Revenue extends Component
             return $this->admin_render();
         } else {
             $my_user_ids = DB::table('brand_users')->where('brand_id', auth()->user()->brand_list())->pluck('user_id')->toArray();
-            $buh_users = User::whereIn('is_employee', [0, 4])->whereIn('id', $my_user_ids)->orderBy('name', 'ASC')
+            $buh_users = User::whereIn('is_employee', [0, 4, 6])->whereIn('id', $my_user_ids)->orderBy('name', 'ASC')
                 ->get();
-            dump('auth()->user()->brand_list()', auth()->user()->brand_list());
-            dd('$my_user_ids', $my_user_ids);
         }
 
         $daily_data = [];
@@ -112,7 +110,7 @@ class Revenue extends Component
         $buh_data = [];
         foreach ($buh_users as $buh_user) {
             $my_user_ids = DB::table('brand_users')->where('brand_id', $buh_user->brand_list())->pluck('user_id')->toArray();
-            $sale_agents = User::whereIn('is_employee', [0, 4])->whereIn('id', $my_user_ids)->orderBy('name', 'ASC')
+            $sale_agents = User::whereIn('is_employee', [0, 4, 6])->whereIn('id', $my_user_ids)->orderBy('name', 'ASC')
                 ->get();
 
             $daily_data = [];
