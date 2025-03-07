6d4168accb8550d22e72b73d5fa1c948282cadd0
Shahrukh Siddiqui <shahrukh.siddiqui@technifiedlabs.com>
2025-03-08 00:46:41 +0500

    admin: view brief forms

--- resources/views/livewire/client-detail.blade.php
+++ resources/views/livewire/client-detail.blade.php
@@ -508,6 +508,7 @@
                                                             @endif
 
                                                             @if(\Illuminate\Support\Facades\Auth::user()->is_employee == 2)
+                                                                <td>
                                                                     @if($project->form_checker != 0)
                                                                         <a href="{{ route('admin.form', [ 'form_id' => $project->form_id , 'check' => $project->form_checker, 'id' => $project->id]) }}" class="badge badge-info badge-icon badge-sm">
                                                                             <i class="i-Receipt-4 mr-1"></i>
