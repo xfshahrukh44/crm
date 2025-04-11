21e39dbd224e704e9f3be1d0f8de6e25684c5314
Shahrukh Siddiqui <shahrukh.siddiqui@technifiedlabs.com>
2025-04-12 03:31:37 +0500

    sales: task update flow change

--- resources/views/client/invoice/pay-with-authorize.blade.php
+++ resources/views/client/invoice/pay-with-authorize.blade.php
@@ -560,6 +560,27 @@
                                         </select>
                                     </div>`);
                 $('#state').select2();
+            } else if ($(this).val() == 'CA') {
+                $('#state_wrapper').html(`<label for="state">Province/Territory (code)</label>
+                            <div>
+                                <select class="form-control" name="state" id="state" required>
+                                    <option value="">Select a Province/Territory</option>
+                                    <option value="AB">Alberta</option>
+                                    <option value="BC">British Columbia</option>
+                                    <option value="MB">Manitoba</option>
+                                    <option value="NB">New Brunswick</option>
+                                    <option value="NL">Newfoundland and Labrador</option>
+                                    <option value="NS">Nova Scotia</option>
+                                    <option value="NT">Northwest Territories</option>
+                                    <option value="NU">Nunavut</option>
+                                    <option value="ON">Ontario</option>
+                                    <option value="PE">Prince Edward Island</option>
+                                    <option value="QC">Quebec</option>
+                                    <option value="SK">Saskatchewan</option>
+                                    <option value="YT">Yukon</option>
+                                </select>
+                            </div>`);
+                $('#state').select2();
             } else {
                 $('#state_wrapper').html(`<label for="state">State (code)</label>
                                     <div>
