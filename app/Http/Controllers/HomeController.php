529fd6d9e2e60e7f7879c16b5478e6a5e7de8bf6
xfshahrukh44 <ssidduit@gmail.com>
2025-05-08 21:28:39 +0500

    clear notifications commented

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -101,18 +101,18 @@ class HomeController extends Controller
     }
 
     public function readNotification(Request $request){
-//        $user = Auth::user();
-//        $user->unreadNotifications()->get()->map(function($n) {
-//            $n->markAsRead();
-//        });
+        $user = Auth::user();
+        $user->unreadNotifications()->get()->map(function($n) {
+            $n->markAsRead();
+        });
         return back();
     }
 
     public function markNotification(){
-//        $user = Auth::user();
-//        $user->unreadNotifications()->get()->map(function($n) {
-//            $n->markAsRead();
-//        });
+        $user = Auth::user();
+        $user->unreadNotifications()->get()->map(function($n) {
+            $n->markAsRead();
+        });
         return back();
     }
 
